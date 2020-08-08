51e151c2a443f3ccabc616e82bd551cd8d8deeb6
happy Vova <v.cherepovich@gmail.com>
2020-08-08 14:50:25 +0000

    Reverse direct of deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "barbershop"
+set :repo_url, "git@github.com:cherepovich/barbershop.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/www_barbershop"
+
+set :linked_files, ["config/secrets.yml"]
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+#Vova typed
+server "18.219.151.21", :user => "deploy", :roles => %{web app}
 
 
 # role-based syntax
@@ -20,7 +22,6 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
 
 
 # Configuration
